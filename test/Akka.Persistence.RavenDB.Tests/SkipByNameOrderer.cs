6d643f7c71e2a4010cf519285309ece7282801c9
Karmel Indych <karmeli87@gmail.com>
2024-06-17 14:16:23 +0300

    skip performance tests

---
+++ test/Akka.Persistence.RavenDB.Tests/SkipByNameOrderer.cs
@@ -0,0 +1,25 @@
+﻿using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: Xunit.TestCaseOrderer("Akka.Persistence.RavenDb.Tests.SkipByNameOrderer", "Akka.Persistence.RavenDb.Tests")]
+
+namespace Akka.Persistence.RavenDb.Tests
+{
+    public class SkipByNameOrderer : ITestCaseOrderer
+    {
+        public static readonly bool SkipPerformance;
+        
+        static SkipByNameOrderer()
+        {
+            SkipPerformance = bool.Parse(Environment.GetEnvironmentVariable("SKIP_PERF") ?? "FALSE");
+        }
+
+
+        public SkipByNameOrderer(IMessageSink diagnosticMessageSink)
+        {
+        }
+
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+            => testCases.Where(x => SkipPerformance  == false || x.TestMethod.TestClass.Class.Name.StartsWith(typeof(RavenDbJournalPerfSpec).FullName, StringComparison.Ordinal) == false);
+    }
+}
