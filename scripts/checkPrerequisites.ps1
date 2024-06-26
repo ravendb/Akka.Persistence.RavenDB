8a4c131a27811b89dfc5510c6809fd2078e11a36
Bartosz Piekarski <bartosz@ravendb.net>
2024-06-26 13:51:27 +0200

    HRINT-3675 Make build.ps1 work with older powershell

--- scripts/checkPrerequisites.ps1
+++ scripts/checkPrerequisites.ps1
@@ -7,8 +7,6 @@ function CheckPrerequisites () {
     if ($null -eq $(Get-Command "dotnet" -ErrorAction SilentlyContinue)) {
         throw "dotnet not found in path."
     }
-    
-    CheckLastExitCode
 
     if ($PSVersionTable.PSVersion.Major -lt 5) {
         throw "Incompatible Powershell version. Must be 5 or later."
